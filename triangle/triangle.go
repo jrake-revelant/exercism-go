1ce243bdbb9db431fc6aa7f3d8c9ecbe1fb4b505
Jannis Rake-Revelant <jannis.rake-revelant@zalando.de>
2019-01-04 15:53:44 +0100

    next batch of exercises

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -5,24 +5,38 @@
 // https://golang.org/doc/effective_go.html#commentary
 package triangle
 
+import "math"
 
 // Notice KindFromSides() returns this type. Pick a suitable data type.
-type Kind
+type Kind string
 
 const (
-    // Pick values for the following identifiers used by the test program.
-    NaT // not a triangle
-    Equ // equilateral
-    Iso // isosceles
-    Sca // scalene
+	// Pick values for the following identifiers used by the test program.
+	NaT Kind = "Not A Triangle" // not a triangle
+	Equ Kind = "equilateral"    // equilateral
+	Iso Kind = "isosceles"      // isosceles
+	Sca Kind = "scalene"        // scalene
 )
 
 // KindFromSides should have a comment documenting it.
 func KindFromSides(a, b, c float64) Kind {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
 	var k Kind
+	// default not a triangle
+	k = NaT
+	// filter out NaNs and infinities, I don't know if this is the right thing to do here
+	if (math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c)) || (math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0)) {
+		return k
+	}
+	// Is it really a triangle?
+	if (a > 0 && b > 0 && c > 0) && (a+b >= c && a+c >= b && c+b >= a) {
+		if a == b && b == c {
+			k = Equ
+		} else if a == b || a == c || c == b {
+			k = Iso
+		} else {
+			k = Sca
+		}
+	}
+
 	return k
 }
